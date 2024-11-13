490fbdeb5c4d69b06faf23fa8aed9b99941afe11
Jdelorme <volmer394@gmail.com>
2024-11-13 19:49:42 +0100

    add:some test lines

--- Module00/ex01/contact.hpp
+++ Module00/ex01/contact.hpp
@@ -1,23 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   phonebook.cpp                                      :+:      :+:    :+:   */
+/*   contact.hpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jdelorme <jdelorme@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/13 17:49:55 by jdelorme          #+#    #+#             */
-/*   Updated: 2024/11/13 19:48:52 by jdelorme         ###   ########.fr       */
+/*   Created: 2024/11/13 19:29:28 by jdelorme          #+#    #+#             */
+/*   Updated: 2024/11/13 19:41:31 by jdelorme         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <iostream>
-
-
-int	main()
-{
-	std::string input;
-
-	std::cout << "Bienvenido a tu agenda de contactos. Opciones disponibles ADD, SEARCH, EXIT" << std::endl;
-	std::cin >> input;
-	input.compare
-	}
+ 
--- Module00/ex01/phonebook.hpp
+++ Module00/ex01/phonebook.hpp
@@ -6,7 +6,26 @@
 /*   By: jdelorme <jdelorme@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/13 17:49:53 by jdelorme          #+#    #+#             */
-/*   Updated: 2024/11/13 17:49:54 by jdelorme         ###   ########.fr       */
+/*   Updated: 2024/11/13 19:41:46 by jdelorme         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+//CONTACT.HPP
+//Necesitamos un constructor de contacto
+//Necesitamos un destructor de contacto
+
+//PHONEBOOK.HPP
+//Constructor y destructor
+//
+
+
+class	PhoneBook{
+public:
+	char array[8];
+};
+
+class	Contact{
+public:
+	int number;
+	std::string name; 
+};
