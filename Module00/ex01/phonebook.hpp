459a304e8a07299ef074aef8644d90b046e2e9ce
Jdelorme <volmer394@gmail.com>
2024-11-20 16:05:18 +0100

    add: getters, some variables,

--- Module00/ex01/phonebook.hpp
+++ Module00/ex01/phonebook.hpp
@@ -6,25 +6,23 @@
 /*   By: jdelorme <jdelorme@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/13 17:49:53 by jdelorme          #+#    #+#             */
-/*   Updated: 2024/11/20 13:14:05 by jdelorme         ###   ########.fr       */
+/*   Updated: 2024/11/20 15:55:03 by jdelorme         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//CONTACT.HPP
-//Necesitamos un constructor de contacto
-//Necesitamos un destructor de contacto
+#ifndef PHONBOOK_HPP
+# define PHONEBOOK_HPP
 
-//PHONEBOOK.HPP
-//Constructor y destructor
-//
 #include <iostream>
 #include <string>
 
 #define MAX_CONTACTS 8
 
 class	PhoneBook{
+private:
+	size_t	_size;
+	Contact	*_contact;
 public:
-
 	PhoneBook(void);
 	~PhoneBook(void);
 };
@@ -43,9 +41,13 @@ public:
 	void	setValue(std::string full_name, std::string scnd_name, std::string nickname
 		, std::string ph_number, std::string dark_secret);
 	/*Funciones getter atributos para el setter sobre el contacto*/
-	std::string getFullname(size_t size) const; //		X
-	std::string getScndname(size_t size) const; //		X
-	std::string getNickname(size_t size) const; //		X
-	std::string getPhoneNumber(size_t size) const;//	X
-	std::string getDarkestSecret(size_t size) const;//	X
+	std::string getFullname(size_t size) const; //		D
+	std::string getScndname(size_t size) const; //		D
+	std::string getNickname(size_t size) const; //		D
+	std::string getPhoneNumber(size_t size) const;//	D
+	std::string getDarkestSecret(size_t size) const;//	D
+	/*	Funcion para mostrar la infromación del contacto	*/
+	void	ft_showInfo(void);
 };
+
+#endif
