7adf9730388d7d593bc72bda16c634e8d02aebcb
Jdelorme <volmer394@gmail.com>
2024-11-20 16:03:57 +0100

    add: all geters from contact class made

--- Module00/ex01/phonebook.cpp
+++ Module00/ex01/phonebook.cpp
@@ -6,11 +6,15 @@
 /*   By: jdelorme <jdelorme@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/13 17:49:55 by jdelorme          #+#    #+#             */
-/*   Updated: 2024/11/20 13:08:32 by jdelorme         ###   ########.fr       */
+/*   Updated: 2024/11/20 15:54:22 by jdelorme         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "phonebook.hpp"
+/*		APRATADO PHONEBOOK		*/
+
+
+/*		APARTADO DE CREACIÓN DEL CONTACTO	*/
 
 void	Contact::setValue(std::string full_name, std::string scnd_name, std::string nickname
 		, std::string ph_number, std::string dark_secret)
@@ -22,8 +26,60 @@ void	Contact::setValue(std::string full_name, std::string scnd_name, std::string
 	this->_dsecret = dark_secret;
 }
 
+std::string	Contact::getFullname(size_t size) const{
+	if (this->_fullname.length() < size){
+		std::string full_name = this->_fullname.substr(0, size - 1);
+		full_name.push_back('.');
+		return full_name;
+	}
+	return (this->_fullname);
+}
 
+std::string	Contact::getScndname(size_t size) const{
+	if (this->_scndname.length() < size){
+		std::string scnd_name = this->_scndname.substr(0, size - 1);
+		scnd_name.push_back('.');
+		return (scnd_name);
+	}
+	return (this->_scndname);
+}
 
+std::string	Contact::getNickname(size_t size) const{
+	if (this->_nickname.length() > size){
+		std::string nickname = this->_nickname.substr(0, size -1);
+		nickname.push_back('.');
+		return (nickname);
+	}
+	return (this->_nickname);
+}
+
+std::string	Contact::getPhoneNumber(size_t size) const{
+	if (this->_phnumber.length() > size){
+		std::string ph_number =  this->_phnumber.substr(0, size - 1);
+		ph_number.push_back('.');
+		return (ph_number);
+	}
+	return (this->_phnumber);
+}
+
+std::string Contact::getDarkestSecret(size_t size) const{
+	if (this->_dsecret.length() > size){
+		std::string dark_secret = _dsecret.substr(0, size - 1);
+		dark_secret.push_back('.');
+		return (dark_secret);
+	}
+	return (this->_dsecret);
+}
+
+void	Contact::ft_showInfo(void){
+	std::cout << "\tFirst Name:\t" << this->_fullname << std::endl;
+	std::cout << "\tSecond Name:\t" << this->_scndname << std::endl;
+	std::cout << "\tNickname:\t" << this->_nickname << std::endl;
+	std::cout << "\tPhoneNumber:\t" << this->_phnumber << std::endl;
+	std::cout << "\tDarkest Secret\t" << this->_dsecret << std::endl;
+}
+
+/*		FUNCION MAIN PROGRAMA E INSTANCIADOR	*/
 int	main()
 {
 	PhoneBook	PhoneInstance;
